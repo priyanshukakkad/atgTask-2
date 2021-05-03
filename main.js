9252b8356b182597b583d222fb0c420f15221bef
priyanshukakkad <priyanshu2015kakkad@gmail.com>
2021-05-03 17:44:14 +0530

    done

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-
+// PC
 
 var tl = gsap.timeline({
   defaults: {
-    duration: 1
+    duration: 0.5
   }
-})
-var dotStep = $(".dots-nav");
+});
 
-function dotsfunc(para) {
-
-    // hiding
-    for (let i = 1; i <= dotStep.length; i++) {
-        $("#Dots" + i).removeClass("dot-active");
-    }
-
-    for (let i = 1; i <= para; i++) {
-        $("#Dots" + i).addClass("dot-active");
-    }
-}
 function display_fun1() {
-  tl.from(".left1", {
+  tl.from(".left1-0", {
     y: 50,
     stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-1", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
-  gsap.from('.right6 img', {
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "10 1000",
+      ease: "power3.out"
+    }, '-=0.7')
+  gsap.from('.c6-rp img', {
     opacity: 0,
     duration: 1,
     y: 700
@@ -39,8 +31,7 @@ function display_fun1() {
     duration: 1,
     y: 100
   });
-  console.log("1");
-  $(".container-1").removeClass("display-none");
+  $(".container-6").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-2");
   $(".svg-bg").removeClass("svg-bg-3");
@@ -49,7 +40,7 @@ function display_fun1() {
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-7");
   $(".container-7").addClass("display-none");
-  $(".container-6").addClass("display-none");
+  $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
   $(".container-3").addClass("display-none");
   $(".container-4").addClass("display-none");
@@ -60,15 +51,19 @@ function display_fun1() {
 function display_fun2() {
   tl.from(".left1-1", {
     y: 50,
-    stagger: .7,
+    stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-2", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
-  gsap.from('.right7-img1', {
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "35% 1000",
+      ease: "power3.out"
+    }, '-=0.7')
+  gsap.from('.c7-rp-img1', {
     opacity: 0,
     duration: 1,
     y: -200
@@ -78,7 +73,6 @@ function display_fun2() {
     duration: 1,
     y: 200
   });
-  console.log("1");
   $(".container-7").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-7");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -98,14 +92,18 @@ function display_fun2() {
 function display_fun3() {
   tl.from(".left1-2", {
     y: 50,
-    stagger: .7,
+    stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-3", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "70% 1000",
+      ease: "power3.out"
+    }, '-=0.7')
   gsap.from('.nasa-fitness-app-img2', {
     opacity: 0,
     duration: 1,
@@ -116,8 +114,7 @@ function display_fun3() {
     duration: 1,
     y: 200
   });
-  console.log("1");
-  $(".container-6").removeClass("display-none");
+  $(".container-1").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-2");
   $(".svg-bg").removeClass("svg-bg-3");
@@ -126,7 +123,7 @@ function display_fun3() {
   $(".svg-bg").removeClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-7");
   $(".container-7").addClass("display-none");
-  $(".container-1").addClass("display-none");
+  $(".container-6").addClass("display-none");
   $(".container-2").addClass("display-none");
   $(".container-3").addClass("display-none");
   $(".container-4").addClass("display-none");
@@ -136,14 +133,18 @@ function display_fun3() {
 function display_fun4() {
   tl.from(".left1-3", {
     y: 50,
-    stagger: .7,
+    stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-4", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "100% 1000",
+      ease: "power3.out"
+    }, '-=0.7')
   gsap.from('.img-1', {
     opacity: 0,
     duration: 1,
@@ -169,7 +170,6 @@ function display_fun4() {
     duration: 1,
     y: -100
   });
-  console.log("1");
   $(".container-2").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-2");
   $(".svg-bg").removeClass("svg-bg-6");
@@ -177,7 +177,7 @@ function display_fun4() {
   $(".svg-bg").removeClass("svg-bg-4");
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
-  $(".svg-bg").removeClass("svg-bg-7")
+  $(".svg-bg").removeClass("svg-bg-7");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-6").addClass("display-none");
@@ -189,40 +189,43 @@ function display_fun4() {
 function display_fun5() {
   tl.from(".left1-4", {
     y: 50,
-    stagger: .5,
+    stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-5", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
-  gsap.from('.right3-img-1', {
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "130% 1000",
+      ease: "power3.out"
+    }, '-=0.7')
+  gsap.from('.c3-rp-img-1', {
     opacity: 0,
     duration: 1,
     x: 200
   });
-  gsap.from('.right3-img-2', {
+  gsap.from('.c3-rp-img-2', {
     opacity: 0,
     duration: 1,
     y: 200
   });
-  gsap.from('.right3-img-3', {
+  gsap.from('.c3-rp-img-3', {
     opacity: 0,
     duration: 1,
     y: -300
   });
-  gsap.from('.right3-img-4', {
+  gsap.from('.c3-rp-img-4', {
     opacity: 0,
     duration: 1,
     y: 300
   });
-  gsap.from('.right3-img-5', {
+  gsap.from('.c3-rp-img-5', {
     opacity: 0,
     duration: 1,
     y: -300
   });
-  console.log("1");
   $(".container-3").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-3");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -242,20 +245,23 @@ function display_fun5() {
 function display_fun6() {
   tl.from(".left1-5", {
     y: 50,
-    stagger: .7,
+    stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-6", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
-  gsap.from('.right4-img', {
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "165% 1000",
+      ease: "power3.out"
+    }, '-=0.7')
+  gsap.from('.c4-rp-img', {
     opacity: 0,
     duration: 1,
     y: 700
   });
-  console.log("1");
   $(".container-4").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-4");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -263,7 +269,7 @@ function display_fun6() {
   $(".svg-bg").removeClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
-  $(".svg-bg").removeClass("svg-bg-7");;
+  $(".svg-bg").removeClass("svg-bg-7");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -275,30 +281,33 @@ function display_fun6() {
 function display_fun7() {
   tl.from(".left1-6", {
     y: 50,
-    stagger: .7,
+    stagger: .2,
     opacity: 0
   })
   tl.from(".cwr-7", {
-    y: 50,
-    stagger: .2,
-    opacity: 0,
-  }, "-=1.8")
+      y: 50,
+      stagger: .2,
+      opacity: 0,
+    }, "-=1.8")
+    .to("#Opaque_Ring", {
+      strokeDasharray: "200% 1000",
+      ease: "power4.out"
+    }, '-=0.7')
   gsap.from('.bottom-img', {
     opacity: 0,
     duration: 1,
     y: 100
   });
-  gsap.from('.right5-img1', {
+  gsap.from('.c5-rp-img1', {
     opacity: 0,
     duration: 1,
     y: -200
   });
-  gsap.from('.right5-img2', {
+  gsap.from('.c5-rp-img2', {
     opacity: 0,
     duration: 1,
     y: 200
   });
-  console.log("1");
   $(".container-5").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -306,7 +315,7 @@ function display_fun7() {
   $(".svg-bg").removeClass("svg-bg-4");
   $(".svg-bg").removeClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-1");
-  $(".svg-bg").removeClass("svg-bg-7");;
+  $(".svg-bg").removeClass("svg-bg-7");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -314,8 +323,118 @@ function display_fun7() {
   $(".container-4").addClass("display-none");
   $(".container-6").addClass("display-none");
 }
-$('.owl-carousel').owlCarousel( {
 
+// window.addEventListener('scroll', () => {
+//   var scrolled = window.pageYOffset;
+//   console.log(scrolled);
+//   if (scrolled === 0) {
+//     display_fun1();
+//   } else if (scrolled === 100) {
+//     display_fun2();
+//   } else if (scrolled === 200) {
+//     display_fun3();
+//   } else if (scrolled === 300) {
+//     display_fun4();
+//   } else if (scrolled === 400) {
+//     display_fun5();
+//   } else if (scrolled === 500) {
+//     display_fun6();
+//   } else if (scrolled === 600) {
+//     display_fun7();
+//   }
+// });
+
+var tl0 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-1",
+    trigger: '.wrapper',
+    start: "center 105%",
+    end: "center 80%",
+    // markers: true,
+    onLeave: () => display_fun2(),
+    onEnterBack: () => display_fun1()
+  }
+});
+// tl0
+// .call(display_fun1, ["1"], '-=0.4');
+
+
+
+var tl1 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-2",
+    trigger: '.wrapper',
+    start: "center 85%",
+    end: "center 70%",
+    // markers: true,
+    toggleActions: "restart reset restart reset",
+    onEnterBack: () => display_fun2(),
+    onLeave: () => display_fun3()
+  }
+});
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-3",
+    trigger: '.wrapper',
+    start: "center 70%",
+    end: "center 56%",
+    // markers: true,
+    onEnterBack: () => display_fun3(),
+    onLeave: () => display_fun4()
+  }
+});
+
+var tl3 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-4",
+    trigger: '.wrapper',
+    start: "center 56%",
+    end: "center 43%",
+    // markers: true,
+    onEnterBack: () => display_fun4(),
+    onLeave: () => display_fun5()
+  }
+});
+
+var tl4 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-5",
+    trigger: '.wrapper',
+    start: "center 43%",
+    end: "center 30%",
+    // markers: true,
+    onEnterBack: () => display_fun5(),
+    onLeave: () => display_fun6()
+  }
+});
+
+var tl5 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-6",
+    trigger: '.wrapper',
+    start: "center 30%",
+    end: "center 15%",
+    // markers: true,
+    onEnterBack: () => display_fun6(),
+    onLeave: () => display_fun7()
+  }
+});
+
+var tl6 = gsap.timeline({
+  scrollTrigger: {
+    // id: "slide-7",
+    trigger: '.wrapper',
+    start: "center 15%",
+    end: "center -2%",
+    // markers: true,
+    onEnterBack: () => display_fun7(),
+    onLeave: () => display_fun1()
+  }
+});
+
+// MOBILE
+$('.owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
   autoplay: true,
@@ -325,21 +444,15 @@ $('.owl-carousel').owlCarousel( {
     0: {
       items: 1,
       nav: true
-    }
-
-    ,
+    },
     768: {
       items: 1,
       nav: false
-    }
-
-    ,
+    },
     1000: {
       items: 5,
       nav: true,
       loop: false
     }
   }
-}
-
-)
+})
